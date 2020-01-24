595525e53b12ef90e69b126c5051e940d58af9b3
Henry <cajapasto5@gmail.com>
2020-01-24 17:30:34 -0300

    prueba

--- proyectoDatawarehouse/Data/admin.py
+++ proyectoDatawarehouse/Data/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Producto,Precio
+from .models import Producto,Precio,UnidaddeMedida
 admin.site.register(Producto)
 admin.site.register(Precio)
+admin.site.register(UnidaddeMedida)
 # Register your models here.
